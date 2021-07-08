9c4da86f12d5d5789edb04f307df61840f7159a8
Nazaret <nazaret155@mail.ru>
2021-07-08 19:18:56 +0300

    added btn back

--- src/components/pages/item-page.js
+++ src/components/pages/item-page.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import WithRestoService from '../hoc';
 import Error from '../error'
 import {menuLoaded, menuRequested, menuErrored, addedToCart, changingTotal} from '../../actions';
+import { Link } from 'react-router-dom';
 
 import './item-page.css';
 import Spinner from '../spinner';
@@ -43,16 +44,19 @@ class ItemPage extends Component {
         const {title, url, category, price, id} = item
 
         return(
-            <div className = "item_page">
-                <div className="menu__item item_block">
-                    <div className="menu__title">{title}</div>
-                    <img className="menu__img" src={url} alt={title}></img>
-                    <div className="menu__category">Category: <span>{category}</span></div>
-                    <div className="menu__price">Price: <span>{price}$</span></div>
-                    <button onClick={() => {this.props.addedToCart(id); this.props.changingTotal()}} className="menu__btn">Add to cart</button>
-                    <span className = {`menu__category_Img ${category}`}></span> 
+            <>
+                <div className = "item_page">
+                    <div className="menu__item item_block">
+                        <div className="menu__title">{title}</div>
+                        <img className="menu__img" src={url} alt={title}></img>
+                        <div className="menu__category">Category: <span>{category}</span></div>
+                        <div className="menu__price">Price: <span>{price}$</span></div>
+                        <button onClick={() => {this.props.addedToCart(id); this.props.changingTotal()}} className="menu__btn">Add to cart</button>
+                        <Link to='/' className="menu__btn btn-back">Вернуться в меню</Link>
+                        <span className = {`menu__category_Img ${category}`}></span> 
+                    </div>
                 </div>
-            </div>
+            </>
         )
     }
 }
