dde8c83459de418caa9e6aa0542a609c0caa106f
Nazaret <nazaret155@mail.ru>
2021-07-10 23:48:21 +0300

    email validation

--- src/components/utils/validators.js
+++ src/components/utils/validators.js
@@ -7,3 +7,11 @@ export const maxLengthCreator = maxLength => value => {
     if(value && value.length > maxLength) return `Максимальное количество символов ${maxLength}`;
     return undefined
 }
+
+export const emailValidation = value => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(value).toLowerCase())) {
+        return 'некорректный email'
+    }
+    return undefined
+}
