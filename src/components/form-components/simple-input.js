3e76af2283487c34c4b356bd4c8844606bfc93ca
Nazaret <nazaret155@mail.ru>
2021-07-10 23:50:29 +0300

    added form components, for redux-form

---
+++ src/components/form-components/simple-input.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const SimpleInput = ({input, meta, ...props}) => {
+
+    const error = meta.touched && meta.error;
+    let styles;
+    if (error) {
+        styles = {
+            border: 'solid crimson 2px'
+        }
+    }
+
+    console.log(meta);
+
+    return(
+        <div>
+            <div>
+                <input {...input} {...props} style={styles}/>
+            </div>
+            {error && <span style={{color: 'crimson', fontSize: 14}}>{meta.error}</span>}
+        </div>
+    )
+} 
+
+export default SimpleInput;
