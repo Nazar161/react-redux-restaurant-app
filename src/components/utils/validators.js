205e1097045a8a226057ba0f30e3630aceed2a25
Nazaret <nazaret155@mail.ru>
2021-07-18 17:05:47 +0300

    bug fix

--- src/components/utils/validators.js
+++ src/components/utils/validators.js
@@ -10,7 +10,7 @@ export const maxLengthCreator = maxLength => value => {
 
 export const emailValidation = value => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!re.test(String(value).toLowerCase())) {
+    if (!re.test(String(value).toLowerCase()) && value) {
         return 'некорректный email'
     }
     return undefined
